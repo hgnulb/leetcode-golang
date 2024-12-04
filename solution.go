839e74db49269a04933805eb94ca50a860074e0a
hgnulb <hgnulb@163.com>
2024-12-04 14:21:49 +0800

    no message

--- solution.go
+++ solution.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World!")
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Next: head}
+	slow, fast := dummy, dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
 }
