9557967d4ed7a0c54fc46bfff400561c31a7c412
hgnulb <hgnulb@163.com>
2025-03-20 18:53:33 +0800

    no message

--- solution.go
+++ solution.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 type user struct {
 	name string
 	age  uint64
 }
 
 func main() {
-	u := []user{
-		{"asong", 23},
-		{"song", 19},
-		{"asong2020", 18},
-	}
-	n := make([]*user, 0, len(u))
-
-	for _, v := range u {
-		o := v
-		n = append(n, &o)
-	}
-	fmt.Println(n)
-
-	for _, v := range n {
-		fmt.Println(v)
-	}
 }
